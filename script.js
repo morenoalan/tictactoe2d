3bb418a5b895e7b47d95f237ae39364a534cac12
Alan Moreno <alan.moreno@hotmail.com>
2022-11-25 23:51:06 -0300

    added score count

--- script.js
+++ script.js
@@ -8,30 +8,46 @@ const winConditions = [
     ['tile1', 'tile5', 'tile9'],
     ['tile3', 'tile5', 'tile7'],
 ];
-
 var playersData = [
     {
         'player':'X',
         'name':'John',
-        'markedTiles':[]
+        'markedTiles':[],
+        'score': 0
     },
     {
         'player':'O',
         'name':'Mary',
-        'markedTiles':[]
+        'markedTiles':[],
+        'score': 0
     }
 ];
+
+var statusGame = 'playing';
+function StartGame(){
+    statusGame = 'playing';
+    for(let i = 0; i < playersData.length; i++){
+        playersData[i].markedTiles = [];
+    }
+    let tilesList = document.getElementsByClassName('tile');
+    for(let j = 0; j < tilesList.length; j++){
+        tilesList[j].innerHTML = '';
+    }
+}
 
 function VerifyVictory(){
     for(let i=0; i<winConditions.length; i++){
         if(winConditions[i].every(elem => playersData[0].markedTiles.includes(elem))){
+            playersData[0].score += 1;
             console.log(playersData[0].name+' wins!');
+            console.log(playersData.find(item => item.player == 'X').name +' '+ playersData.find(item => item.player == 'X').score +' x ' + playersData.find(item => item.player == 'O').score + ' ' + playersData.find(item => item.player == 'O').name);
+            statusGame = 'stopped';
         }
     }
 }
 
 function Mark(tile){
-    if(tile.innerHTML == ''){
+    if(tile.innerHTML == '' && statusGame == 'playing'){
         tile.innerHTML = playersData[0].player;
         playersData[0].markedTiles.push(tile.getAttribute('id'));
         console.log(playersData[0].markedTiles);
